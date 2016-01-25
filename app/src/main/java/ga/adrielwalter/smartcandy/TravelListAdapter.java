9c75a6e664f8cc782fd5ae4d83368191cb7979ac
adrielstar <adri_star1986@hotmail.com>
2016-01-25 23:16:30 +0100

    Using the Palette API in the List

--- app/src/main/java/ga/adrielwalter/smartcandy/TravelListAdapter.java
+++ app/src/main/java/ga/adrielwalter/smartcandy/TravelListAdapter.java
@@ -1,6 +1,9 @@
 package ga.adrielwalter.smartcandy;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,7 +44,7 @@ public class TravelListAdapter extends RecyclerView.Adapter<TravelListAdapter.Vi
         @Override
         public void onClick(View v) {
             if (mItemClickListener != null) {
-                mItemClickListener.onItemClick(itemView, getAdapterPosition());
+                mItemClickListener.onItemClick(itemView, getLayoutPosition());
             }
         }
     }
@@ -70,5 +73,15 @@ public class TravelListAdapter extends RecyclerView.Adapter<TravelListAdapter.Vi
         final Place place = new PlaceData().placeList().get(position);
         holder.placeName.setText(place.name);
         Picasso.with(mContext).load(place.getImageResourceId(mContext)).into(holder.placeImage);
+
+        Bitmap photo = BitmapFactory.decodeResource(mContext.getResources(), place.getImageResourceId(mContext));
+
+        Palette.from(photo).generate(new Palette.PaletteAsyncListener() {
+            public void onGenerated(Palette palette) {
+                int bgColor = palette.getMutedColor(mContext.getResources().getColor(android.R.color.black));
+                holder.placeNameHolder.setBackgroundColor(bgColor);
+            }
+        });
     }
+
 }
